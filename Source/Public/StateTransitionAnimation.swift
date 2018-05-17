87248a8239667fb82dc6e0a3d02d2f37ddb8bf7c
nevillco <connor.neville@raizlabs.com>
2018-05-17 19:43:35 -0400

    Remove unused variables

--- Source/Public/StateTransitionAnimation.swift
+++ Source/Public/StateTransitionAnimation.swift
@@ -15,23 +15,5 @@ public enum StateTransitionAnimation {
     case appearAndSimultaneouslyRemove(onAppear: StateTransitionAnimationOptions?, onRemove: StateTransitionAnimationOptions?)
     case removePreviousThenAppear(onRemove: StateTransitionAnimationOptions?, onAppear: StateTransitionAnimationOptions?)
 
-    var additionOptions: StateTransitionAnimationOptions? {
-        switch self {
-        case .appearOverPrevious(let onAppear): return onAppear
-        case .appearUnderPrevious: return nil
-        case .appearAndSimultaneouslyRemove(let onAppear, _): return onAppear
-        case .removePreviousThenAppear(_, let onAppear): return onAppear
-        }
-    }
-
-    var removalOptions: StateTransitionAnimationOptions? {
-        switch self {
-        case .appearOverPrevious: return nil
-        case .appearUnderPrevious(let onRemove): return onRemove
-        case .appearAndSimultaneouslyRemove(_, let onRemove): return onRemove
-        case .removePreviousThenAppear(let onRemove, _): return onRemove
-        }
-    }
-
 }
 
