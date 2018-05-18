b083077e06807c6d7584a23d50d95c61e19d8500
nevillco <connor.neville@raizlabs.com>
2018-05-18 18:23:34 -0400

    Add empty StatefulNavigationController

---
+++ Example/Tests/StatefulNavigationControllerSpec.swift
@@ -0,0 +1,18 @@
+//
+//  StatefulNavigationControllerSpec.swift
+//  Reinstate_Tests
+//
+//  Created by Connor Neville on 5/18/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import Reinstate
+
+class StatefulNavigationControllerSpec: QuickSpec {
+
+    override func spec() {
+    }
+
+}
